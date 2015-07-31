32ed4d7ddfdd3fb8ca9b7c8520951d27b360382f
Faith Coldren <fcoldren@gmail.com>
2015-07-31 10:23:44 -0400

    paired two-way model added

--- anovasplicecalc.R
+++ anovasplicecalc.R
@@ -1,25 +1,40 @@
 # this function should receive an object of type
 # list that has at lease three columns:
 # "Exon", "Group", and "Counts"
+# for paired test, a fourth column "Sample" is required
 
-twowayanova <- function(exons){
+twowayanova <- function(exons, aovtype = "unpaired"){
 # this function calculates a two-way ANOVA for a balanced, unpaired design
 # check that required column names present  
 if(!("Counts" %in% colnames(exons)) | !("Exon" %in% colnames(exons)) | !("Group" %in% colnames(exons)))
 {
-  stop("Exon, Group, and Counts must be column headers\n", call. = TRUE);
+  stop("Exon, Group, and Counts must be column headers\n", call. = FALSE);
 }
 
-# to-do: if not balanced, do not go further
 b <- replications(Counts ~ Exon*Group, data = exons)
 if(!(typeof(b) == "integer"))
 {
-  stop("Experiment appears to be unbalanced, ANOVA will not be run\n", call. = TRUE)
+  stop("Experiment appears to be unbalanced, ANOVA will not be run\n", call. = FALSE)
 }
 
-# run two-way aov
-int <- aov(exons$Counts~exons$Exon*exons$Group)
+# select two-way aov test to run
+if(aovtype == "unpaired")
+{ int <- aov(exons$Counts~exons$Exon*exons$Group) 
+  pvalues <- summary(int)[[1]][["Pr(>F)"]][1:3]
+} 
+else if(aovtype == "paired")
+{
+# two-way aov for paired
+# group variable is within subjects
+  if(!("Sample" %in% colnames(exons)))
+  {
+    stop("Sample must be a column header for the paired ANOVA\n", call. = FALSE);
+  }
+  stop("The paired two-way aov is not implemented yet\n", call. = FALSE)
+  #int <- aov(exons$Counts ~ exons$Exon*exons$Group + Error(exons$Sample/exons$Group))
+  # need to figure out which value to return from the paired aov
+}
 
-# Order of returned P-values: Exon, Group, Interaction
-return(summary(int)[[1]][["Pr(>F)"]][1:3])
+# Order of returned P-values for unpaired: Exon, Group, Interaction
+return(pvalues)
 }
